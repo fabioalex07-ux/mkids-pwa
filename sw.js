590738c9bd8006eaff0d10ecc3f1c909f2cfe27d
fabioalex07-ux <fabioalex07@gmail.com>
2026-02-04 09:50:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE = 'mkids-pwa-v3';
-
-self.addEventListener('install', e => {
+self.addEventListener('install', event => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ])
-    )
-  );
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Offline', {
+        status: 503,
+        statusText: 'Offline'
+      });
+    })
   );
 });
