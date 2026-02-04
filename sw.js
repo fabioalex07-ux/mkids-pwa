1b3266b26e874f696d56c4ef4d9e745f523deaa3
fabioalex07-ux <fabioalex07@gmail.com>
2026-02-04 12:11:33 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,39 @@
+const CACHE_NAME = 'mkids-pwa-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response('Offline', {
-        status: 503,
-        statusText: 'Offline'
-      });
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
-<script>
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/mkids-pwa/sw.js')
-      .then(() => console.log('SW registrado'))
-      .catch(err => console.error('SW error', err));
-  });
-}
-</script>
